e8e5c10d7ea99ccd4f90c8c1636bbb82438d37ce
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-01 18:36:01 -0300

    commit 6

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,15 +1,12 @@
 import 'dart:async';
-
+import 'package:flutter/material.dart';
 import 'package:app_lista_tarefas/components/appbar_header.dart';
-import 'package:app_lista_tarefas/components/lista_carregamento_mensagem.dart';
-import 'package:app_lista_tarefas/components/lista_vazia_mensagem.dart';
 import 'package:app_lista_tarefas/components/titulo.dart';
 import 'package:app_lista_tarefas/data/tarefa_dao.dart';
 import 'package:app_lista_tarefas/models/tarefa_model.dart';
 import 'package:app_lista_tarefas/pages/nova_tarefa.dart';
 import 'package:app_lista_tarefas/styles/cores.dart';
 import 'package:app_lista_tarefas/styles/fonts.dart';
-import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,82 +16,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  StreamController<List<TarefaModel>> streamController =
+      StreamController<List<TarefaModel>>();
+
   @override
   Widget build(BuildContext context) {
-    StreamController<List<TarefaModel>> _streamController =
-        StreamController<List<TarefaModel>>();
+    void addDataToStream() async {
+      List<TarefaModel> newData = await TarefaDao().findAll();
 
-    var dados = Stream.fromFuture(TarefaDao().findAll());
+      streamController.sink.add(newData);
+    }
+
+    addDataToStream();
 
     return Scaffold(
       appBar: const AppBarHeader(titulo: "Lista de tarefas"),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
+        padding: const EdgeInsets.only(left: 24, right: 24, top: 24),
         child: StreamBuilder<List<TarefaModel>>(
-            stream: dados,
+            stream: streamController.stream,
+            // stream: Stream.fromFuture(TarefaDao().findAll()),
             builder: (context, snapshot) {
-              List<TarefaModel>? tarefas = snapshot.data;
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                  return const ListaCarregamentoMensagem();
-                // break;
-                case ConnectionState.waiting:
-                  return const ListaCarregamentoMensagem();
-                // break;
-                case ConnectionState.active:
-                  return const ListaCarregamentoMensagem();
-                // break;
-                case ConnectionState.done:
-                  if (snapshot.hasData && tarefas != null) {
-                    if (tarefas.isNotEmpty) {
-                      return ListView.builder(
-                        padding: const EdgeInsets.only(top: 24, bottom: 70),
-                        itemCount: tarefas.length,
-                        itemBuilder: (context, index) {
-                          TarefaModel item = tarefas[index];
-                          String tarefa = "${item.tarefa.substring(0, 40)}...";
-                          String autor = item.autor;
-                          String dataHora = item.dataCriacao;
+              if (snapshot.hasData) {
+                List<TarefaModel>? tarefas = snapshot.data;
 
-                          return Container(
-                            margin: const EdgeInsets.only(bottom: 10),
-                            child: ListTile(
-                              title: Text(tarefa),
-                              subtitle: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(autor),
-                                  Text(dataHora),
-                                ],
-                              ),
-                              shape: const RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(8)),
-                                side: BorderSide(color: Colors.black, width: 1),
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    }
-                    return const ListaVaziaMensagem();
-                  }
-                  return const Titulo(
-                    titulo: "Erro ao carregar a lista de contatos",
-                    color: vermelho,
-                    alignment: Alignment.center,
-                    fontSize: 24,
-                    fontWeight: fontWeightBold,
-                  );
-                default:
-                  return const Titulo(
-                    titulo: "Erro desconhecido!!!",
-                    color: vermelho,
-                    alignment: Alignment.center,
-                    fontSize: 32,
-                    fontWeight: fontWeightBold,
-                  );
+                return ListView.builder(
+                  padding: const EdgeInsets.only(bottom: 70),
+                  itemCount: tarefas!.length,
+                  itemBuilder: (context, index) {
+                    TarefaModel item = tarefas[index];
+
+                    return Tarefa(tarefa: item);
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return const Titulo(
+                  titulo: "Erro ao carregar a lista de tarefas",
+                  color: vermelho,
+                  alignment: Alignment.center,
+                  fontSize: 24,
+                  fontWeight: fontWeightBold,
+                );
+              } else {
+                return const CircularProgressIndicator();
               }
             }),
       ),
@@ -110,6 +74,80 @@ class _HomePageState extends State<HomePage> {
         tooltip: "Adicionar",
         shape: const CircleBorder(),
         child: const Icon(Icons.add, color: branco),
+      ),
+    );
+  }
+}
+
+class Tarefa extends StatefulWidget {
+  const Tarefa({
+    super.key,
+    required this.tarefa,
+  });
+
+  final TarefaModel tarefa;
+
+  @override
+  State<Tarefa> createState() => _TarefaState();
+}
+
+class _TarefaState extends State<Tarefa> {
+  bool _isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    // _isChecked = (widget.tarefa.concluido == "Sim") ? true : false;
+
+    var item = widget.tarefa;
+
+    updateCheckbox(value) {
+      setState(() {
+        TarefaDao().update(
+            TarefaModel(
+              id: item.id,
+              tarefa: item.tarefa,
+              autor: item.autor,
+              concluido: (_isChecked) ? "Não" : "Sim",
+              dataCriacao: item.dataCriacao,
+            ),
+            item.id);
+        _isChecked = value!;
+      });
+    }
+
+    return Container(
+      // key: Key(uuid),
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        title: Text("${item.tarefa.substring(0, 40)}..."),
+        subtitle: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(item.autor),
+            Text(item.dataCriacao),
+            Text(item.concluido),
+          ],
+        ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          side: BorderSide(color: Colors.black, width: 1),
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+        leading: Checkbox(
+          value: _isChecked,
+          onChanged: updateCheckbox,
+        ),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {
+                TarefaDao().delete(item.id);
+              },
+              icon: const Icon(Icons.delete),
+            ),
+          ],
+        ),
       ),
     );
   }
